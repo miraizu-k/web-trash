d2a1d6389afb5598d45975cbe0edebba6f9f63eb
romer <romer.t.co@gmail.com>
2012-01-26 16:54:05 +0900

    added drop events

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -3,11 +3,38 @@ var socket = io.connect(),
 
 socket.on('connect',function () {
     id = socket.socket.sessionid;
+    var filelist = $("#filelist");
     
-    socket.on(message,function (data) {
-        $("#filelist").empty();
+    filelist.parent()
+        .on('drop',function (e) {
+            return false;
+        })
+        .on('dropenter dragover',function () {
+            return false;
+        });
+
+    socket.on(id,function (data) {
+        filelist.empty().trigger("add-files");
         $.each(data,function (index,fileName) {
-            $("li")
+            $("li").text(fileName).appendTo(filelist);
         });
     });
+});
+
+$(function () {
+    var dropArea = $('#target'),
+        h2 = dropArea.find('> h2');
+    dropArea.on('drop',function (e) {
+        return false;
+    });
+    h2.css({
+        'marginTop' : "-" + (h2.outerHeight() / 2) + "px",
+        'marginLeft': "-" + (h2.outerWidth() / 2) + "px",
+        'opacity'   : '0.6'
+    });
+    $("#filelist")
+        .empty()
+        .on('add-files',function () {
+            h2.fadeTo(1000,'0.1');
+        });
 });
